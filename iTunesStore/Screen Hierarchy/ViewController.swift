3550f329e4fcb440bc465af6c90014a9f34bcfeb
LinnierGames <esericksanc@gmail.com>
2018-05-16 13:57:33 -0700

    noone: Xcode Project
    Setup sub folders

---
+++ iTunesStore/Screen Hierarchy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iTunesStore
+//
+//  Created by Erick Sanchez on 5/16/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+}
+
