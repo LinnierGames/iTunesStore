913e4c76fb2e28d6b5ba0dfea984b8a97bf31019
LinnierGames <esericksanc@gmail.com>
2018-05-16 19:47:20 -0700

    Update Styles in `MovieDetailedVc`

--- Podfile
+++ Podfile
@@ -13,6 +13,10 @@ target 'iTunesStore' do
   
   #
   pod 'Kingfisher'
+  
+  # Color framework for Swift & Objective-C (Gradient colors, hexcode support, colors from images & more).
+  pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
 
   target 'iTunesStoreTests' do
     inherit! :search_paths
--- iTunesStore/MovieDetailViewController.swift
+++ iTunesStore/MovieDetailViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Kingfisher
+import ChameleonFramework
+import UIDesignables
 
 class MovieDetailViewController: UIViewController {
     
@@ -27,14 +29,37 @@ class MovieDetailViewController: UIViewController {
     
     // MARK: - VOID METHODS
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        buttonPrice.cornerRadius = buttonPrice.bounds.height / 2.0
+        buttonShare.cornerRadius = buttonShare.bounds.height / 2.0
+    }
+    
     private func updateUI() {
         guard self.movie != nil else {
             fatalError("movie was not set by a parent view controller")
         }
         
-        imageHeadline.kf.setImage(with: movie.compUrl)
         labelTitle.text = movie.title
         buttonPrice.setTitle(movie.price, for: .normal)
+        
+        imageHeadline.kf.setImage(with: movie.compUrl) { [weak self] (image, error, _, _) in
+            guard let unwrappedSelf = self, let image = image else { return }
+            
+            //change the colors of the buttons to match the image
+            let averageColor = UIColor(averageColorFrom: image)
+            let contrastingColor = UIColor(contrastingBlackOrWhiteColorOn: averageColor, isFlat: true)
+            UIView.animate(withDuration: 0.25, animations: {
+                unwrappedSelf.buttonPrice.backgroundColor = averageColor
+                unwrappedSelf.buttonPrice.tintColor = contrastingColor
+                unwrappedSelf.buttonShare.backgroundColor = averageColor
+                unwrappedSelf.buttonShare.tintColor = contrastingColor
+                
+                unwrappedSelf.buttonBack.tintColor = contrastingColor
+            })
+        }
+        
         let formattedDateString = movie.releaseDate.formattedStringWith(
             .Month_fullName, " ", .Day_ofTheMonthNoPadding, ", ", .Year_noPadding
         )
@@ -46,17 +71,19 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var imageHeadline: UIImageView!
     @IBOutlet weak var labelTitle: UILabel!
     
-    @IBOutlet weak var buttonPrice: UIButton!
+    @IBOutlet weak var buttonPrice: UIBootton!
     @IBAction func pressBuyOniTunes(_ sender: Any) {
         
     }
     
+    @IBOutlet weak var buttonShare: UIBootton!
     @IBAction func pressShare(_ sender: Any) {
         
     }
     
     @IBOutlet weak var labelReleaseDate: UILabel!
     
+    @IBOutlet weak var buttonBack: UIButton!
     @IBAction func pressBack(_ sender: Any) {
         self.navigationController!.popViewController(animated: true)
     }
