9f1dd5ebe621aec3b073cad5b81eaaa5fd94ef88
LinnierGames <esericksanc@gmail.com>
2018-05-16 17:38:29 -0700

    Populate Movies in UITableView

---
+++ iTunesStore/MovieTableViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  MovieTableViewCell.swift
+//  iTunesStore
+//
+//  Created by Erick Sanchez on 5/16/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    private(set) var titleLabel: UILabel!
+    
+    private(set) var priceLabel: UILabel!
+    
+    private(set) var releaseDateLabel: UILabel!
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        initLayout()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        initLayout()
+    }
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    func configure(_ movie: Movie) {
+        titleLabel.text = movie.title
+        priceLabel.text = movie.price
+        releaseDateLabel.text = String(describing: movie.releaseDate)
+    }
+    
+    private func initLayout() {
+        let horzStackView = UIStackView()
+        horzStackView.axis = .vertical
+        
+        //title label
+        titleLabel = UILabel()
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24.0)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        horzStackView.addArrangedSubview(titleLabel)
+        
+        //price and release date
+        let vertStackView = UIStackView()
+        vertStackView.axis = .horizontal
+        
+        //price
+        priceLabel = UILabel()
+        
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        vertStackView.addArrangedSubview(priceLabel)
+        
+        //release date
+        releaseDateLabel = UILabel()
+        releaseDateLabel.textAlignment = .right
+        
+        releaseDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        vertStackView.addArrangedSubview(releaseDateLabel)
+        
+        //layout
+        vertStackView.translatesAutoresizingMaskIntoConstraints = false
+        vertStackView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        horzStackView.addArrangedSubview(vertStackView)
+        
+        horzStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(horzStackView)
+        horzStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        horzStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        horzStackView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor).isActive = true
+        layoutMarginsGuide.trailingAnchor.constraint(equalTo: horzStackView.trailingAnchor, constant: 8).isActive = true
+    }
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+
+}
--- iTunesStore/UIMoviesTableViewController.swift
+++ iTunesStore/UIMoviesTableViewController.swift
@@ -10,7 +10,33 @@ import UIKit
 
 class MoviesTableViewController: UITableViewController {
     
+    private var movies = [Movie]()
+    
+    private lazy var network = NetworkingStack()
+    
     // MARK: - RETURN VALUES
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56.0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movie cell", for: indexPath) as! MovieTableViewCell
+        
+        let movie = self.movies[indexPath.row]
+        cell.configure(movie)
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
     
     // MARK: - VOID METHODS
     
@@ -22,6 +48,24 @@ class MoviesTableViewController: UITableViewController {
         super.viewDidLoad()
         
         self.title = "Top Movies"
+        
+        tableView.register(MovieTableViewCell.self, forCellReuseIdentifier: "movie cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        network.topMoves { (result) in
+            switch result {
+            case .success(let movies):
+                self.movies = movies
+                self.tableView.reloadData()
+            case .failure:
+                let alertError = UIAlertController(title: "iTunes Store", message: "failed to download movies", preferredStyle: .alert)
+                alertError.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                self.present(alertError, animated: true)
+            }
+        }
     }
 
 }
